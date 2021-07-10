af8b42ed722cb2499276ba9eaea231219186c1d7
dulangakithmini <dulangakithmini@gmail.com>
2021-07-11 00:27:29 +0530

    Wrap the MaterialApp with StreamProvider

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,17 @@
+import 'package:coffee_crew/models/user_model.dart';
 import 'package:coffee_crew/screens/wrapper.dart';
+import 'package:coffee_crew/services/auth_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Wrapper(),
+    return StreamProvider<UserModel>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
     );
   }
 }
