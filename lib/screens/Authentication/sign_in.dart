cdfb0e31089dc2067304d49b37836dce34dbca8d
dulangakithmini <dulangakithmini@gmail.com>
2021-07-10 23:09:54 +0530

    Create a user model

--- lib/screens/Authentication/sign_in.dart
+++ lib/screens/Authentication/sign_in.dart
@@ -1,4 +1,4 @@
-import 'package:coffee_crew/services/auth.dart';
+import 'package:coffee_crew/services/auth_service.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
