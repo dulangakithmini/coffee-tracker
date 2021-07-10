d177d5d2befc9313190239d0f4fe4dcb61b6a5a6
dulangakithmini <dulangakithmini@gmail.com>
2021-07-10 19:58:52 +0530

    Create AuthenticationScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,6 @@
+import 'package:coffee_crew/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(App());
-}
-
-class App extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Coffee Crew'),
-        ),
-      ),
-    );
-  }
 }
