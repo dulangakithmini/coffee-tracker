d965236dafcca491051b8a52f396671669d72ff0
dulangakithmini <dulangakithmini@gmail.com>
2021-07-11 16:01:33 +0530

    Create sign in form

--- lib/screens/Authentication/sign_in.dart
+++ lib/screens/Authentication/sign_in.dart
@@ -20,17 +20,53 @@ class _SignInState extends State<SignIn> {
       ),
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: ElevatedButton(
-          child: Text('Sign in Anonymously'),
-          onPressed: () async {
-            dynamic result = await _auth.signInAnon();
-            if (result == null) {
-              print('Error signing in!');
-            } else {
-              print('signed in');
-              print(result);
-            }
-          },
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                onChanged: (val) {},
+              ),
+              TextFormField(
+                onChanged: (val) {},
+                obscureText: true,
+              ),
+              SizedBox(height: 20.0),
+              ElevatedButton(
+                child: Text(
+                  'Sign in',
+                  style: TextStyle(
+                    fontSize: 17,
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.pink[400],
+                ),
+                onPressed: () async {},
+              ),
+              // ElevatedButton(
+              //   child: Text(
+              //     'Sign in Anonymously',
+              //     style: TextStyle(
+              //       fontSize: 17,
+              //       color: Colors.black,
+              //       fontWeight: FontWeight.w600,
+              //     ),
+              //   ),
+              //   style: ElevatedButton.styleFrom(
+              //     primary: Colors.white,
+              //   ),
+              //   onPressed: () async {
+              //     dynamic result = await _auth.signInAnon();
+              //     if (result == null) {
+              //       print('Error signing in!');
+              //     } else {
+              //       print('signed in');
+              //       print(result);
+              //     }
+              //   },
+              // ),
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/Home/home_screen.dart
+++ lib/screens/Home/home_screen.dart
@@ -1,6 +1,9 @@
+import 'package:coffee_crew/services/auth_service.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
+  final AuthService _auth = AuthService();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,7 +15,9 @@ class HomeScreen extends StatelessWidget {
           elevation: 0.0,
           actions: <Widget>[
             OutlinedButton.icon(
-              onPressed: () {},
+              onPressed: () async {
+                await _auth.signOut();
+              },
               icon: Icon(Icons.person),
               label: Text('Log out'),
               style: OutlinedButton.styleFrom(
