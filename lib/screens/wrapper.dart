3dccea22f01498dfbb3c09cc0ab39a1b53c9cec1
dulangakithmini <dulangakithmini@gmail.com>
2021-07-11 13:18:54 +0530

    Complete the Wrapper class

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,10 +1,20 @@
+import 'package:coffee_crew/models/user_model.dart';
 import 'package:coffee_crew/screens/Authentication/authentication_screen.dart';
+import 'package:coffee_crew/screens/Home/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<UserModel>(context);
+    print(user);
+
     /// return either home or aythentication
-    return AuthenticationScreen();
+    if (user == null) {
+      return AuthenticationScreen();
+    } else {
+      return HomeScreen();
+    }
   }
 }
