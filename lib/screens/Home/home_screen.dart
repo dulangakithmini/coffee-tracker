7a7dcaad8e1977534070e43e206e0bd47d9116b5
dulangakithmini <dulangakithmini@gmail.com>
2021-07-11 13:23:01 +0530

    Add logout button to the HomeScreen

--- lib/screens/Home/home_screen.dart
+++ lib/screens/Home/home_screen.dart
@@ -5,8 +5,21 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.brown[50],
         appBar: AppBar(
           title: Text('Coffee Crew'),
+          backgroundColor: Colors.brown[400],
+          elevation: 0.0,
+          actions: <Widget>[
+            OutlinedButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.person),
+              label: Text('Log out'),
+              style: OutlinedButton.styleFrom(
+                primary: Colors.black,
+              ),
+            ),
+          ],
         ),
       ),
     );
